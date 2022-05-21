ce40493fd4dee8fd94207e319365ad525ec20abc
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-05-21 18:21:49 +0100

    update latest svelte and add playwright test

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,19 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+
+const mode = process.env.NODE_ENV;
+const dev = mode === "development";
+
+const config = {
+  plugins: [
+    tailwindcss(), // first load TailwindCSS
+    autoprefixer(), // then run autoprefixer
+    !dev && // optimize the code for production
+    cssnano({
+      preset: "default",
+    }),
+  ],
+};
+
+module.exports = config;
