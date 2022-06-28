1ab0c0a5b4c990e449d01a9b0ec96e234a82d5ad
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-06-28 18:00:07 +0100

    add lib and components

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,27 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import path from 'path';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: preprocess({
+    postcss: true,
+  }),
 
   kit: {
     adapter: adapter(),
+    appDir: 'internal',
+    vite: {
+      resolve: {
+        alias: {
+          $components: path.resolve('./src/components'),
+          $lib: path.resolve('./src/lib'),
+        },
+      },
+    },
   },
 };
 
